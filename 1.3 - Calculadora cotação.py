6ae39961c4943a569b68298a8cb622db3eabcd32
Yuri <77989191+Champozo@users.noreply.github.com>
2021-01-26 14:19:38 -0300

    Update 1.3 - Calculadora cotação.py

--- 1.3 - Calculadora cotação.py
+++ 1.3 - Calculadora cotação.py
@@ -1,5 +1,19 @@
-real = float(input('Quanto dinheiro vc tem? R$: '))
-cotacao = float(input('Qual a cotação atual do cotacao? '))
-dolar = real / cotacao
+print('Bem vindo a calculadora de cotação.')
+try:
+  real = float(input('Quanto dinheiro vc tem? R$: '))
+except ValueError:
+  print('Digite apenas números inteiros.')
+  real = float(input('Quanto dinheiro vc tem? R$: '))
+escolha = str(input('Que moeda gostaria de comprar? (dolar/euro)'))
 
-print(f'Com R$: {real} você pode comprar U$: {dolar}')
+if escolha == 'dolar':
+    cotacao = float(input('Qual a cotação atual do dolar? '))
+    dolar = real / cotacao
+    print(f'Com R$: {real} você pode comprar U$: {dolar:.2f}')
+elif escolha == 'euro':
+    cotacao = float(input('Qual a cotação atual do euro? '))
+    euro = real / cotacao
+    print(f'Com R$: {real} você pode comprar €{euro:.2f}')
+else:
+    print('Não entendi. Por favor digite se quer comprar dolar ou euro.')
+
